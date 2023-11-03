c5f77b93bcc49cb71f60acba2327069200463519
KahMeng15 <85149963+KahMeng15@users.noreply.github.com>
2023-11-03 19:33:27 +0800

    hi raziq 3

--- src/com/zetcode/LeaderboardManager.java
+++ src/com/zetcode/LeaderboardManager.java
@@ -1,66 +1,122 @@
-
 package com.zetcode;
 
 import javax.swing.*;
-import java.awt.Font;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.List;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class LeaderboardManager {
-    public static void displayLeaderboard(int highScore) {
-        JFrame frame = new JFrame("Leaderboard");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static boolean saver = false;
+    private static JFrame frame;
+    private static JFrame framev;
+    
+
+    public static boolean displayLeaderboard(int highScore, String playerName) {
+        framev = new JFrame("Leaderboard");
+        framev.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         List<String> leaderboardData = loadLeaderboardData("leaderboard.txt");
 
-        String playerName = JOptionPane.showInputDialog("Enter your name: ");
-        int playerScore = highScore;
+        String newEntry = playerName + ": " + highScore;
 
-        String newEntry = playerName + ": " + playerScore;
-        leaderboardData.add(newEntry);
+        boolean scoreAdded = false;
 
-        Collections.sort(leaderboardData, (s1, s2) -> {
-            int score1 = Integer.parseInt(s1.split(": ")[1]);
-            int score2 = Integer.parseInt(s2.split(": ")[1]);
-            return Integer.compare(score2, score1); // Descending order
-        });
+        if (leaderboardData.isEmpty() || leaderboardData.size() < 5) {
+            leaderboardData.add(newEntry);
+            scoreAdded = true;
+        } else {
+            int lowestScore = Integer.MAX_VALUE;
+            int lowestScoreIndex = -1;
 
-        int maxEntries = 5;
-        if (leaderboardData.size() > maxEntries) {
-            leaderboardData = leaderboardData.subList(0, maxEntries);
+            for (int i = 0; i < leaderboardData.size(); i++) {
+                int score = Integer.parseInt(leaderboardData.get(i).split(": ")[1]);
+                if (score < lowestScore) {
+                    lowestScore = score;
+                    lowestScoreIndex = i;
+                }
+            }
+
+            if (highScore > lowestScore) {
+                leaderboardData.remove(lowestScoreIndex);
+                leaderboardData.add(newEntry);
+                scoreAdded = true;
+            }
         }
 
-        DefaultListModel<String> leaderboardModel = new DefaultListModel<>();
-        for (String entry : leaderboardData) {
-            leaderboardModel.addElement(entry);
+        if (scoreAdded) {
+            Collections.sort(leaderboardData, (s1, s2) -> {
+                int score1 = Integer.parseInt(s1.split(": ")[1]);
+                int score2 = Integer.parseInt(s2.split(": ")[1]);
+                return Integer.compare(score2, score1); // Descending order
+            });
+
+            DefaultListModel<String> leaderboardModel = new DefaultListModel<>();
+            for (int i = 0; i < leaderboardData.size(); i++) {
+                String entry = leaderboardData.get(i);
+                String[] parts = entry.split(": ");
+                String name = parts[0];
+                int score = Integer.parseInt(parts[1]);
+                String rank = Integer.toString(i + 1); // Ranking numbers
+
+                // Use a custom renderer to format and style the leaderboard
+                String formattedEntry = String.format("  %-2s %-15s %s", rank, name, score);
+                leaderboardModel.addElement(formattedEntry);
+            }
+
+            JList<String> leaderboardList = new JList<>(leaderboardModel);
+
+            // Use a monospaced font for consistent alignment
+            leaderboardList.setFont(new Font("Monospaced", Font.BOLD, 16));
+            leaderboardList.setForeground(Color.YELLOW);
+            leaderboardList.setBackground(Color.BLACK);
+            leaderboardList.setSelectionBackground(Color.RED);
+
+            JScrollPane scrollPane = new JScrollPane(leaderboardList);
+            frame.add(scrollPane);
+            
+            // Calculate the frame height based on the number of entries
+            int numEntries = leaderboardModel.getSize();
+            int frameHeight = Math.min(600, numEntries * 22 + 50); // Limit max height
+            
+            framev.setLocation(650, 320);
+            ImageIcon licon = new ImageIcon("src/resources/images/licon.png");
+            framev.setIconImage(licon.getImage());
+            
+            framev.setSize(300, frameHeight); // Set frame size
+            
+            framev.setVisible(true);
+            
+            // Close the frame when 'L' key is pressed
+//        JRootPane rootPane = SwingUtilities.getRootPane(frame);
+//        rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_L, 0), "closeFrame");
+//        rootPane.getActionMap().put("closeFrame", new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                frame.dispose();
+//            }
+//        });
+
+            saver = saveLeaderboardData(leaderboardData, "leaderboard.txt");
+        } else {
+            JOptionPane.showMessageDialog(null, "Sorry, your score is too low!");
         }
 
-        JList<String> leaderboardList = new JList<>(leaderboardModel);
-
-        // Use a smaller font size
-        leaderboardList.setFont(new Font("Arial", Font.PLAIN, 14));
-
-        frame.add(new JScrollPane(leaderboardList));
-
-        frame.setSize(200, 250);
-        frame.setVisible(true);
-
-        saveLeaderboardData(leaderboardData, "leaderboard.txt");
+        return true;
     }
 
-    private static List<String> loadLeaderboardData(String fileName) {
+   private static List<String> loadLeaderboardData(String fileName) {
         List<String> leaderboardData = new ArrayList<>();
         try {
             File file = new File(fileName);
             if (file.exists()) {
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    leaderboardData.add(line);
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        leaderboardData.add(line);
+                    }
                 }
-                reader.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -68,18 +124,63 @@ public class LeaderboardManager {
         return leaderboardData;
     }
 
-    private static void saveLeaderboardData(List<String> leaderboardData, String fileName) {
+    private static boolean saveLeaderboardData(List<String> leaderboardData, String fileName) {
         try {
             File file = new File(fileName);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            for (String entry : leaderboardData) {
-                writer.write(entry);
-                writer.newLine();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                for (String entry : leaderboardData) {
+                    writer.write(entry);
+                    writer.newLine();
+                }
             }
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
+    
+   public static void displayLeaderboard() {
+        frame = new JFrame("Leaderboard");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        List<String> leaderboardData = loadLeaderboardData("leaderboard.txt");
+        Collections.sort(leaderboardData, (s1, s2) -> Integer.compare(
+                Integer.parseInt(s2.split(": ")[1]),
+                Integer.parseInt(s1.split(": ")[1])
+        ));
+
+        DefaultListModel<String> leaderboardModel = new DefaultListModel<>();
+        for (int i = 0; i < leaderboardData.size(); i++) {
+            String entry = leaderboardData.get(i);
+            String[] parts = entry.split(": ");
+            String rank = Integer.toString(i + 1);
+            leaderboardModel.addElement(String.format("  %-2s %-15s %s", rank, parts[0], parts[1]));
+        }
+
+        JList<String> leaderboardList = new JList<>(leaderboardModel);
+        leaderboardList.setFont(new Font("Monospaced", Font.BOLD, 16));
+        leaderboardList.setForeground(Color.YELLOW);
+        leaderboardList.setBackground(Color.BLACK);
+        leaderboardList.setSelectionBackground(Color.RED);
+
+        frame.add(new JScrollPane(leaderboardList));
+        int frameHeight = Math.min(600, leaderboardModel.getSize() * 22 + 50);
+        frame.setSize(300, frameHeight);
+        frame.setLocation(650, 320);
+        frame.setIconImage(new ImageIcon("src/resources/images/licon.png").getImage());
+        frame.setVisible(true);
+
+        // Close the frame when 'L' key is pressed
+        JRootPane rootPane = SwingUtilities.getRootPane(frame);
+        rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_L, 0), "closeFrame");
+        rootPane.getActionMap().put("closeFrame", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+        saveLeaderboardData(leaderboardData, "leaderboard.txt");
+    }
 }
